40c2130ea372f019d502a6ee85692b352915d64e
Turner Bohlen <turnerbohlen@gmail.com>
2012-12-09 12:33:58 -0500

    Adds landing page

--- web/main.py
+++ web/main.py
@@ -1,12 +1,13 @@
 """
-
+Flask server
 """
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template("index.html", hello=True);
 
 if __name__ == "__main__":
     app.run()
