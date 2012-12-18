04c9b71c7102886b0862ec3792900e9448204eca
Troy Astorino <troyastorino@gmail.com>
2012-12-17 22:45:03 -0500

    added log likelihood function

--- web/analysis.py
+++ web/analysis.py
@@ -1,6 +1,12 @@
 import db
 import imageGen
 from numpy import *
+from scipy.stats import t
+
+mu0 = 255 / 256.0 # prior mean
+l0 = 1 # confidence in prior mean
+sig_sq0 = (256 / 4.0)**2 # prior variance
+a0 = 1 # confidence in prior variance
 
 """
 Function: get_image_matrix
@@ -26,20 +32,71 @@ def get_image_matrix(image_id):
     else:
         image_file = db.get_image_file(image_id)
         image_matrices[image_id] = array(imageGen.loadImage(image_file))
-        return imate_matrices[image_id]
+        return image_matrices[image_id]
 
+"""
+Function: log_likelihood
+
+Calculates the log_likelihood of observering the set of features of the current
+object given that it is from the group group_num
+
+Parameters:
+current_partition - the current partitioning of the other images
+group_num - the group that the current image would be assigned to
+move - the move dict holding the information about the current move
+"""
+def log_likelihood(current_partition, group_num, move):
+    # the images grouped so far
+    images = images_in_group(current_partition, group_num)
+
+    # number of images
+    n = len(images)
+
+    # matrix for image
+    image_matrix = get_image_matrix(move['image_id'])
+
+    # get the shape for the group
+    image_shape = image_matrix.shape
+    shape = (n, image_shape[0], image_shape[0])
+    
+    # get the matricies for all the images in the group
+    group = empty(shape)
+    for i in range(n):
+        group[i] = get_image_matrix(images[i])
+
+    # calculate parameters common to all dimensions
+    l = l0 + n
+    a = a0 + n
+
+    # get mean, variance matrix
+    group_mean = mean(group, axis=0)
+    group_var = var(group, axis=0)
+
+    # calculate the parameters of the student_t distribution
+    mu = (l0 * mu0 + n * group_mean) / l
+    sig_sq = (a0 * sig_sq0 + (n - 1) * group_var +
+              l0 * n * (mu0 - group_mean) ** 2 / l) / a
+    scale = sig_sq * (1 + 1 / l)
+
+    # calculate the log probability over each dimension
+    log_p = t.logpdf(image, a, loc=mu, scale=scale)
+
+    return sum(log_p)
+    
 """
 Function: move_probability
 
 Calculates the probability of a given move
 
 Parameters:
-current_partition - representation of the current partition fo the images.  Is a dict where each key is an image ID, and each value is the group number that image is in
+current_partition - representation of the current partition for the images.  Is a dict where each key is an image ID, and each value is the group number that image is in
 move - dict of the move being made. Same form as in db
+
+Returns:
+The probability of the move
 """
 def move_probability(current_partition, move):
     pass
-    
 
 """
 Function: compare_trial
@@ -47,7 +104,6 @@ Compares the moves in a trial to the moves that a particle filter would have mad
 
 Parameters:
 trial_id - the ObjectId of the trial to analyze
-image_set_matricies - a dict of matrices corresponding to an image set. Should be the output of get_image_set_matrices()
 move_probability - a function for calculating the probability of a given move.  Takes arguments of current_partition and move, where current_partition is a dict of image_id to group number, and move is a dict for a move
 
 Returns:
